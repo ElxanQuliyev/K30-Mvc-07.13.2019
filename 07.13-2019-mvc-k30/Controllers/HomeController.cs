40c1bff4ab8fc467457638fa457b8420cb85e2c0
Elxan <qelxan177@gmail.com>
2019-07-14 17:03:08 +0400

    Mvc Website

---
+++ 07.13-2019-mvc-k30/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using _07._13_2019_mvc_k30.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _07._13_2019_mvc_k30.Controllers
+{
+    public class HomeController : Controller
+    {
+        K30FinanceDBEntities db = new K30FinanceDBEntities();
+        public ActionResult Index()
+        {
+            ViewBag.topSection = db.TopSection1.First();
+            ViewBag.section2 = db.Section2Divs.ToList();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
